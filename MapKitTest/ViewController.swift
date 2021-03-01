40793533e26f5b7387900efc94b34c66fd3893a8
Egor <lacost765123@gmail.com>
2021-03-01 19:54:04 +0300

    add delegate method

--- MapKitTest/ViewController.swift
+++ MapKitTest/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     var locationManager: CLLocationManager = CLLocationManager()
@@ -22,8 +22,16 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.distanceFilter = kCLDistanceFilterNone
         locationManager.startUpdatingLocation()
         
+        locationManager.delegate = self
         mapView.showsUserLocation = true
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
 }
 
